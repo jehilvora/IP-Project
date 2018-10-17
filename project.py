d76f82576c924ed1737cd9cd49425eb31545cc7a
jehilvora <jehilvora@gmail.com>
2018-10-17 17:28:33 +0530

    login functionality added

--- project.py
+++ project.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+import os
 
 app = Flask(__name__)
-
-# Root link
-@app.route("/",methods=['GET'])
-def homePage():
-	return "<h1>HomePage</h1>"
+app.secret_key = os.urandom(12)
 
 # Route for handling login page
-@app.route("/login", methods=['POST'])
+@app.route("/", methods=['GET'])
+def home():
+	if session.get("logged_in"):
+		return redirect(url_for('homePage'))
+	else:
+		return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return "<h1>Login Page</h1>"
+	userName = 'admin'
+	if request.form['username'] == userName and request.form['password'] == 'password':
+		session['username'] = userName
+		session['logged_in'] = True
+		return redirect(url_for('homePage'))
+	else:
+		return "<h2>Invalid ID</h2>"
+
+@app.route("/homepage", methods=['GET'])
+def homePage():
+	return render_template("homepage.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
